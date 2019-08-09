796b61d61b37ac1f6eed115a945fd2ef31753e07
ManuelPllqr <manuelpellequer@gmail.com>
2019-08-09 13:48:55 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Box, Button, Collapsible, Drop, TextArea, Heading, RadioButton, Grommet
 import { FormClose, Notification, Facebook } from 'grommet-icons';
 import { LineChart, RadarChart} from 'grommet-controls/chartjs';
 import { hpe } from 'grommet-theme-hpe';
-
+{/*import libraries above*/}
 
 
 const AppBar = (props) => (
@@ -19,7 +19,7 @@ const AppBar = (props) => (
 
 );
 
-
+{/*sets theme and HPE color id*/}
 const theme = {
   global: {
     colors: {
@@ -37,17 +37,24 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = { name: '' }
+    {/*this statements are used to update the text typed by the user inside the date input text boxes*/}
     this.handleStartDateChange = this.handleStartDateChange.bind(this);
     this.handleEndDateChange = this.handleEndDateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.targetRef = React.createRef();
     this.state = { value: 0 };
     this.state = { value: 'dy/mm/year' }
+    {/*default value inside date textboxes*/}
+    {/* below are the functions used for the date user input textboxes */}
   }
+
+
 
   handleStartDateChange(startdate){
     this.setState({startdate});
   }
+
+
 
   handleEndDateChange(enddate){
     this.setState({enddate});
@@ -70,17 +77,18 @@ class App extends Component {
 
 
           <Box fill>
-
-            <AppBar flex margin={{top:'7%'}}  gap='30%'>
+            {/*direction 'row' allows elements inside the box to be aligned horizontally, gap sets the gap between elements of the box*/}
+            <AppBar flex margin={{top:'7%'}}  gap='10%'>
               <Image
                   style={{width:100,height:120}}
                   src='https://www.hpe.com/etc/designs/hpeweb/logo.jpg'
-              />
+              /> {/*link to hpe logo*/}
+              {/*alignSelf and textAlign centers the text*/}
               <Heading level='3' size='small' color='#00b388' alignSelf='center' textAlign='center'>Predictive Maintenance - Analytics</Heading>
               <Image
                   style={{width:50,height:50}}
                   src='https://media.licdn.com/dms/image/C4D0BAQEZ2_r-IM1lRw/company-logo_200_200/0?e=2159024400&v=beta&t=cbDc9rSI649EBVMWOgbddgWmR4yItafGXzOL_t2TmdM'
-              />
+              /> {/*link to dataiku image*/}
               <Box align='center'>
                 <Box
                     background='dark-4'
@@ -91,26 +99,35 @@ class App extends Component {
                 >
                 </Box>
 
+                {/*Drop ;enu defined below*/}
                 {this.targetRef.current && (
+
                     <Drop flex
                         align={{ top: 'bottom', left: 'left' }}
                         target={this.targetRef.current}
                     >
+                      {/*pad is the space between box elements and the box border*/}
                       <Box pad='medium' flex gap='5%'>
                         <Heading level='5' textAlign='center'>DataIku Scenario</Heading>
                         <Text textAlign='center' size='9px'>From Date</Text>
+                        {/*default value inside the textarea set zith defaultValue=*/}
+                        {/* value = {this.state.enddate} updates value of text inside textarea*/}
+                        {/* onChangeText={this.handleEndDateChange} actions performed when the text inside the textarea is modified*/}
                         <TextArea
                             defaultValue = 'dy/mm/year'
                             value = {this.state.enddate}
                             onChangeText={this.handleEndDateChange}
                         />
                         <Text textAlign='center' size='9px'>To Date</Text>
+                        {/* value={this.state.startdate} updates value of text inside textarea*/}
+                        {/*onChangeText={this.handleStartDateChange} actions performed when the text inside the textarea is modified*/}
                         <TextArea
                             defaultValue='dy/mm/year'
                             value={this.state.startdate}
                             onChangeText={this.handleStartDateChange}
                         />
                         <Text textAlign='center' size='9px'>Scenario Measure</Text>
+                        {/*RadioButtons are below with label indicating the text that will be displayed on the buttons*/}
                         <RadioButton flex size='xsmall'
                                      label="volt"
                         />
@@ -123,13 +140,16 @@ class App extends Component {
                         <RadioButton flex size='xsmall'
                                      label="vibration"
                         />
+                        {/* active={true} button can be pressed if active is true, if we change it false the button can't be pressed*/}
+                        {/* onClick={() => alert('Clicked')} action performed when the button is clicked*/}
                         <Button flex
                             color='green'
                             active={true}
                             label='Clear Selection'
                             onClick={() => alert('Clicked')}
                         />
-
+                        {/*onPress={this.handleSubmit} updates information that the button has been pressed with this statement*/}
+                        {/* onClick={() => alert('Clicked')} action performed when the button is clicked*/}
                         <Button
                             color='green'
                             active={true}
@@ -160,6 +180,7 @@ class App extends Component {
             <Box direction='row' gap='5%' style={{width:'60%',height:'10%'}} margin={{left:'3%', bottom:'3%'}}>
               <Heading level='3' size="9px">Chart1 Measure</Heading>
               <Box gap='10px'>
+                {/*toggle={true} allows checkbox to be changed zhen clicked on*/}
                 <CheckBox
                     label='volt'
                     toggle={true}
@@ -193,9 +214,9 @@ class App extends Component {
             </Box>
 
 
-
+            {/*first data chart defined below*/}
             <Box  direction='row' overflow={{ horizontal: 'hidden' }} gap='1%' style={{width:'65%',height:'300%'}}>
-
+              {/*data defined below, horizontal labels are also defined*/}
               <LineChart
                   data={{
                     labels: ["01/01/2016","01/02/2016","01/03/2016","01/04/2016","01/05/2016","01/06/2016","01/07/2016","01/08/2016","01/09/2016","01/10/2016","01/11/2016","01/12/2016"],
@@ -273,7 +294,7 @@ class App extends Component {
 
 
             </Box>
-
+            {/*second data chart defined below*/}
             <Box direction='row' overflow={{ horizontal: 'hidden' }} gap='1%' style={{width:'65%',height:'300%'}}>
               <LineChart
                   data={{
@@ -366,7 +387,7 @@ class App extends Component {
                 />
               </Box>
             </Box>
-
+            {/*third data chart defined below*/}
             <Box direction='row' overflow={{ horizontal: 'hidden' }} style={{width:'65%',height:'300%'}}>
               <LineChart
                   data={{
@@ -425,7 +446,7 @@ class App extends Component {
               />
             </Box>
 
-
+            {/*fourth data chart defined below*/}
             <Box direction='row' overflow={{ horizontal: 'hidden' }} style={{width:'65%',height:'300%'}}>
               <LineChart
                   data={{
